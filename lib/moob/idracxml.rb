ca92c86795da0cabdd06aa44b1b438fad6ec5480
Nathan Ferch <nf@spotify.com>
2015-11-03 15:04:38 -0500

    foo

--- lib/moob/idracxml.rb
+++ lib/moob/idracxml.rb
@@ -1,12 +1,8 @@
 require 'nokogiri'
-require 'tempfile'
-require 'time'
 
 module Moob
 class IdracXml < BaseLom
   @name = 'Dell iDrac XML'
-
-  DISCOVER_XML_NODES = ['ENDPOINTTYPE', 'ENDPOINTVER', 'PROTOCOLTYPE', 'PROTOCOLVER']
 
   def initialize hostname, options = {}
     super hostname, options
@@ -20,19 +16,29 @@ class IdracXml < BaseLom
   def xml_request method, uri, data
     out = @session.send(method, uri, data)
 
-    raise ResponseError.new resp unless out.status == 200
+    raise ResponseError.new out unless out.status == 200
 
     out_xml = Nokogiri::XML(out.body)
-
     raise "Cannot parse XML response for request to #{uri}" unless out_xml
 
     resp_xml = out_xml.xpath("//RESP")
-
     raise "Cannot find response XML node in response to #{uri}" unless resp_xml
 
+    resp = {}
+    resp[:body] = out.body
+    resp[:parsed_xml] = {}
+
     resp_xml.children.each do |n|
-      puts n.inspect
+      resp[:parsed_xml][n.name] = n.content
     end
+    if resp[:parsed_xml].include?('RC')
+      resp[:rc] = Integer(resp[:parsed_xml]['RC'])
+      if resp[:rc] != 0
+        Moob.warn "Return code from #{uri} is #{resp[:rc]}"
+      end
+    end
+
+    return resp
   end
 
   def authenticate
@@ -40,71 +46,36 @@ class IdracXml < BaseLom
     resp = xml_request 'post', 'cgi-bin/login',
       "<?xml version='1.0'?><LOGIN><REQ><USERNAME>#{@username}</USERNAME><PASSWORD>#{@password}</PASSWORD></REQ></LOGIN>"
 
-    raise "Session ID missing from response" unless resp.include?('SID')
+    raise "Auth failed with: \"#{resp[:body]}\"" unless resp[:rc] == 0
+    raise "Session ID missing from response" unless resp[:parsed_xml].include?('SID')
 
-    @sid = resp['SID']
+    @sid = resp[:parsed_xml]['SID']
     @session.headers['Cookie'] = "sid=#{@sid}"
-    raise "Auth failed with: \"#{auth.body}\"" unless @sid.to_i != 0
     return self
   end
 
   def discover
-    out = @session.get 'cgi-bin/discover'
+    resp = xml_request 'get', 'cgi-bin/discover', {}
 
-    raise ResponseError.new out unless out.status == 200
-
-    out_xml = Nokogiri::XML(out.body)
-
-    raise "XML parse error" unless out_xml
-    xml_result = {}
-
-    DISCOVER_XML_NODES.each do |n|
-      n_xml = out_xml.xpath("//#{n}")
-      raise "Can't find #{n} node in XML" unless n_xml
-      xml_result[n] = n_xml.first.content
-    end
-
-    raise "Unsupported iDRAC" unless xml_result['ENDPOINTTYPE'] =~ /^iDRAC[7-8]?$/
-    raise "Unsupported iDRAC subversion" unless xml_result['ENDPOINTVER'] == '1.00'
-    raise "Unsupported protocol type" unless xml_result['PROTOCOLTYPE'] == 'HTTPS'
-    raise "Unsupported protocol version" unless xml_result['PROTOCOLVER'] == '2.0'
+    raise "Unsupported iDRAC" unless resp[:parsed_xml]['ENDPOINTTYPE'] =~ /^iDRAC[7-8]?$/
+    raise "Unsupported iDRAC subversion" unless resp[:parsed_xml]['ENDPOINTVER'] == '1.00'
+    raise "Unsupported protocol type" unless resp[:parsed_xml]['PROTOCOLTYPE'] == 'HTTPS'
+    raise "Unsupported protocol version" unless resp[:parsed_xml]['PROTOCOLVER'] == '2.0'
   end
 
-
   def logout
-    out = @session.get 'cgi-bin/logout'
-    raise ResponseError.new out unless out.status == 200
+    out = xml_request 'get', 'cgi-bin/logout', {}
     return self
   end
 
   action :exec, 'Execute a command'
   def exec
-    req = @session.post '/cgi-bin/exec',
+    out = xml_request 'post', '/cgi-bin/exec',
       "<?xml version='1.0'?><EXEC><REQ><CMDINPUT>#{@arg}</CMDINPUT><MAXOUTPUTLEN>0x0fff</MAXOUTPUTLEN></REQ></EXEC>"
 
-    out_xml = Nokogiri::XML(req.body)
-
-    raise "XML parse error" unless out_xml
-
-    rc_xml = out_xml.xpath("//CMDRC")
-
-    raise "Cannot find RC XML node" unless rc_xml
-
-    rc_s = rc_xml.first.content
-
-    rc = Integer(rc_s)
-
-    output_xml = out_xml.xpath("//CMDOUTPUT")
-
-    raise "Cannot find output XML node" unless output_xml
-
-    if rc != 0
-      Moob.warn "exec returned rc #{rc}"
-    end
-    puts output_xml.first.content
+    puts out[:parsed_xml]['CMDOUTPUT']
     return nil
   end
-
 
 end
 end
